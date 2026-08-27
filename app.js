e80183d816593c6ed630f6a2a3bf8e13893ad3c2
Vishal Gupta <vg4693@gmail.com>
2026-08-28 03:37:03 +0530

    fix root route and port

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ const listingRouter = require("./routes/listing");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 
 main()
@@ -94,9 +93,9 @@ app.get("/demouser", async (req, res) => {
     res.send(registeredUser);
 });
 
-// app.get("/", (req, res) => {
-//     res.send("Hi , I am root");
-// });
+app.get("/", (req, res) => {
+    res.send("Hi , I am root");
+});
 
 // Teeno routers ko yahan connect kiya gaya hai
 app.use("/listings", listingRouter);
